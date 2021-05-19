694af0677df49639e152670ab7db55231e23b55c
Martin Kuehne <martinkuehne@live.de>
2021-05-19 12:03:14 +0200

    finished models, now on to controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Golden Honey', address: 'London', category: 'french')
+Restaurant.create(name: 'Home', address: 'Paris', category: 'chinese')
+Restaurant.create(name: 'Great Escape', address: 'Barcelona', category: 'japanese')
+Restaurant.create(name: 'Osteria Francescana', address: 'Modena', category: 'italian')
+Restaurant.create(name: 'Leffe Beer', address: 'Milan', category: 'belgian')
